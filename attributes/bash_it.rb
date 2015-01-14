e349fc539a54137589a7bd01b209356dd68d1cca
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-01-14 07:42:40 +0100

    Remove skylab

--- attributes/bash_it.rb
+++ attributes/bash_it.rb
@@ -11,7 +11,7 @@ node.default['bash_it'] ={
         bash_it/custom/base.bash
         bash_it/custom/exports.bash
         bash_it/custom/functions.bash
-        bash_it/custom/skylab.completion.bash
+        bash_it/custom/rbenv.bash
       ]
   },
   'theme' => 'roderik',
